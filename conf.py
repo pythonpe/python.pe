fa0cbdfde12c7ab8915dfae5a4b8481f6635888d
Sergio Infante <rsinfante@gmail.com>
2012-11-09 22:17:37 -0500

    Agregando favicon.ico y cambiando a 2012 en footer

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Python Peru'
-copyright = u'2011, Comunidad Python Perú'
+copyright = u'2012, Comunidad Python Perú'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
